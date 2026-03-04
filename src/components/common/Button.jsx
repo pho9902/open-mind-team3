1f925e7b2f131d6e247fca8accc079dd7dd498f9
Hanpla <hanpla25@gmail.com>
2026-03-04 18:54:37 +0900

    fix: 버튼 호버시 테두리 밀려나는 현상 수정

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+export const BasicButton = styled.button`
+  ${({ theme }) => theme.typography.body3}
+  color: ${({ theme }) => theme.colors.gray10};
+  background-color: ${({ theme }) => theme.colors.brown40};
+
+  height: 46px;
+  padding: 8px 12px;
+  border-radius: 8px;
+  border: 2px solid transparent;
+  gap: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    border: 2px solid ${({ theme }) => theme.colors.brown50};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.brown50};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.brown30};
+    cursor: not-allowed;
+  }
+
+  @media (min-width: 768px) {
+    height: 46px;
+    padding: 12px 24px;
+  }
+`;
+
+export const SecondButton = styled.button`
+  ${({ theme }) => theme.typography.caption1Regular}
+  color: ${({ theme }) => theme.colors.brown40};
+  background-color: ${({ theme }) => theme.colors.brown10};
+
+  height: 34px;
+  padding: 8px 12px;
+  border: 1px solid ${({ theme }) => theme.colors.brown40};
+  border-radius: 8px;
+  gap: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.brown40};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.brown20};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.brown10};
+    border: 1px solid ${({ theme }) => theme.colors.brown30};
+
+    cursor: not-allowed;
+  }
+
+  @media (min-width: 768px) {
+    height: 46px;
+    padding: 12px 24px;
+  }
+`;
+
+export const FloatingButton = styled.button`
+  ${({ theme }) => theme.typography.body1}
+  color: ${({ theme }) => theme.colors.gray10};
+  background-color: ${({ theme }) => theme.colors.brown40};
+
+  height: 54px;
+  padding: 12px 24px;
+  border: 1px solid transparent;
+  border-radius: 200px;
+  gap: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
