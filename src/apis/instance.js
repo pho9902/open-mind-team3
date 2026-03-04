eb05bec20fd8fb0b8df7a455274e7f1c97ee3914
pho9902 <bagh9902@gmail.com>
2026-03-04 15:23:13 +0900

    feat: baseURL env 파일에서 불러오게 변경

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://openmind-api.vercel.app/23-3/",
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
