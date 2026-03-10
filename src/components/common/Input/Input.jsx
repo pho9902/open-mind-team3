4836a3e49cedb016a311c913bda2b802e7dea41d
Hanpla <hanpla25@gmail.com>
2026-03-10 17:02:13 +0900

    refactor: 공용 인풋에 ref를 전달하기 위해 fowardRef 추가

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,48 +1,50 @@
+import { forwardRef } from "react";
+
 import * as S from "./Input.style";
 import { theme } from "@/styles/theme";
 
-export const Input = (props) => (
+export const Input = forwardRef((props, ref) => (
   <S.InputContainer>
-    <S.StyledInput {...props} />
+    <S.StyledInput {...props} ref={ref} />
   </S.InputContainer>
+));
+
+export const IconInput = forwardRef(
+  (
+    { leftIcon: LeftIcon, rightIcon: RightIcon, onRightClick, ...props },
+    ref,
+  ) => (
+    <S.InputContainer>
+      {LeftIcon && (
+        <S.IconWrapper>
+          <LeftIcon size={20} color={theme.colors.gray40} />
+        </S.IconWrapper>
+      )}
+      <S.StyledInput {...props} ref={ref} />
+      {RightIcon && (
+        <S.IconWrapper
+          onClick={onRightClick}
+          style={{ cursor: onRightClick ? "pointer" : "default" }}
+        >
+          <RightIcon size={20} color={theme.colors.gray40} />
+        </S.IconWrapper>
+      )}
+    </S.InputContainer>
+  ),
 );
 
-export const IconInput = ({
-  leftIcon: LeftIcon,
-  rightIcon: RightIcon,
-  onRightClick,
-  ...props
-}) => (
-  <S.InputContainer>
-    {LeftIcon && (
-      <S.IconWrapper>
-        <LeftIcon size={20} color={theme.colors.gray40} />
-      </S.IconWrapper>
-    )}
-    <S.StyledInput {...props} />
-    {RightIcon && (
-      <S.IconWrapper onClick={onRightClick}>
-        <RightIcon size={20} color={theme.colors.gray40} />
-      </S.IconWrapper>
-    )}
-  </S.InputContainer>
-);
-
-export const CountInput = ({
-  leftIcon: LeftIcon,
-  current,
-  limit,
-  ...props
-}) => (
-  <S.InputContainer>
-    {LeftIcon && (
-      <S.IconWrapper>
-        <LeftIcon size={20} color={theme.colors.gray40} />
-      </S.IconWrapper>
-    )}
-    <S.StyledInput {...props} maxLength={limit} />
-    <S.InputCount>
-      {current} / {limit}
-    </S.InputCount>
-  </S.InputContainer>
+export const CountInput = forwardRef(
+  ({ leftIcon: LeftIcon, current, limit, ...props }, ref) => (
+    <S.InputContainer>
+      {LeftIcon && (
+        <S.IconWrapper>
+          <LeftIcon size={20} color={theme.colors.gray40} />
+        </S.IconWrapper>
+      )}
+      <S.StyledInput {...props} maxLength={limit} ref={ref} />
+      <S.InputCount>
+        {current} / {limit}
+      </S.InputCount>
+    </S.InputContainer>
+  ),
 );
