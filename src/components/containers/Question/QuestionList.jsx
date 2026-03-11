116562d91711df0b9cf42fe791db3b0a6dc00085
JuHeonParkk <ju10913@naver.com>
2026-03-11 23:52:20 +0900

    refactor: FeedPage 코드 리팩토링-header 이동

--- src/components/containers/Question/QuestionList.jsx
+++ src/components/containers/Question/QuestionList.jsx
@@ -1,21 +1,18 @@
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import { getQuestions } from "@/apis/questions";
 import { MessagesIcon } from "@/assets/icons/MessagesIcon";
 
-import * as S from "./QuestionList.style";
-import FeedHeader from "@/components/containers/Question/FeedHeader/FeedHeader";
 import QuestionCount from "@/components/containers/Question/QuestionCount/QuestionCount";
 import QuestionItems from "@/components/containers/Question/QuestionItems/QuestionItems";
 import PostModal from "@/components/containers/PostModal/PostModal";
+
+import * as S from "@/components/containers/Question/QuestionList.style";
 
 export default function QuestionList({ subjectId, isAnswer }) {
   const [questions, setQuestions] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [isHeaderVisible, setIsHeaderVisible] = useState(false);
-
-  const headerRef = useRef(null);
 
   const fetchQuestions = useCallback(async () => {
     setIsLoading(true);
@@ -33,54 +30,39 @@ export default function QuestionList({ subjectId, isAnswer }) {
     fetchQuestions();
   }, [fetchQuestions]);
 
-  // header 스크롤 감지
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setIsHeaderVisible(!entry.isIntersecting);
-      },
-      { threshold: 0 },
-    );
-    if (headerRef.current) observer.observe(headerRef.current);
-    return () => observer.disconnect();
-  }, [isLoading]);
-
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
     <>
-      <FeedHeader $isScroll={isHeaderVisible} ref={headerRef} />
-      <S.Container>
-        {isAnswer && (
-          <S.ButtonWrapper>
-            <S.DeleteFeedButton
-              onClick={
-                () => console.log("피드 삭제") /* Confrim 띄우고 기능구현 */
-              }
-            >
-              피드 삭제하기
-            </S.DeleteFeedButton>
-          </S.ButtonWrapper>
-        )}
-        <S.QuestionListWrapper>
-          <QuestionCount questions={questions} />
-          <QuestionItems questions={questions} isAnswer={isAnswer} />
-        </S.QuestionListWrapper>
-        <S.QuestionPostButton onClick={() => setIsOpen(true)}>
-          <MessagesIcon size={24} />
-          <span>질문 작성하기</span>
-        </S.QuestionPostButton>
+      {isAnswer && (
+        <S.ButtonWrapper>
+          <S.DeleteFeedButton
+            onClick={
+              () => console.log("피드 삭제") /* Confrim 띄우고 기능구현 */
+            }
+          >
+            피드 삭제하기
+          </S.DeleteFeedButton>
+        </S.ButtonWrapper>
+      )}
+      <S.QuestionListWrapper>
+        <QuestionCount questions={questions} />
+        <QuestionItems questions={questions} isAnswer={isAnswer} />
+      </S.QuestionListWrapper>
+      <S.QuestionPostButton onClick={() => setIsOpen(true)}>
+        <MessagesIcon size={24} />
+        <span>질문 작성하기</span>
+      </S.QuestionPostButton>
 
-        {isOpen && (
-          <PostModal
-            subjectId={subjectId}
-            onClose={() => setIsOpen(false)}
-            onSuccess={fetchQuestions}
-          />
-        )}
-      </S.Container>
+      {isOpen && (
+        <PostModal
+          subjectId={subjectId}
+          onClose={() => setIsOpen(false)}
+          onSuccess={fetchQuestions}
+        />
+      )}
     </>
   );
 }
--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,8 +1,16 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
+
+import FeedHeader from "@/components/containers/Question/FeedHeader/FeedHeader";
 import QuestionList from "@/components/containers/Question/QuestionList";
 
+import * as S from "@/components/containers/Question/QuestionList.style";
+
 export default function FeedPage() {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(false);
+
+  const headerRef = useRef(null);
+
   const navigate = useNavigate();
   const { subjectId, "*": subpath } = useParams();
 
@@ -12,5 +20,24 @@ export default function FeedPage() {
     if (subpath && !isAnswer) navigate("/err", { replace: true });
   }, [subpath, navigate]);
 
-  return <QuestionList subjectId={subjectId} isAnswer={isAnswer} />;
+  /* header scroll observer */
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsHeaderVisible(!entry.isIntersecting);
+      },
+      { threshold: 0 },
+    );
+    if (headerRef.current) observer.observe(headerRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <>
+      <FeedHeader $isScroll={isHeaderVisible} ref={headerRef} />
+      <S.Container>
+        <QuestionList subjectId={subjectId} isAnswer={isAnswer} />
+      </S.Container>
+    </>
+  );
 }
