4d38624c51269a7c4a0f771f2c80bf5c497809f7
Hanpla <hanpla25@gmail.com>
2026-03-11 14:43:47 +0900

    refactor: axios 인터셉터 적용

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,11 +1,37 @@
 import axios from "axios";
 
+const TEAM = "23-3";
+
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 5000,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    if (config.method?.toLowerCase() === "get") {
+      config.params = { team: TEAM, ...config.params };
+    } else if (config.data) {
+      if (config.data instanceof FormData) {
+        if (!config.data.has("team")) {
+          config.data.append("team", TEAM);
+        }
+      } else {
+        config.data = { team: TEAM, ...config.data };
+      }
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    console.error("API Error:", error.response?.data || error.message);
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
