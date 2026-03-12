cf05ca73ac3437c499bbafafea21c9f24d2e0bc3
JuHeonParkk <ju10913@naver.com>
2026-03-12 12:10:39 +0900

    refactor: textarea 글자수에 따른 입력 제한 코드 리팩토링

--- src/components/containers/PostModal/PostModal.jsx
+++ src/components/containers/PostModal/PostModal.jsx
@@ -9,7 +9,7 @@ import { MessagesIcon } from "@/assets/icons/MessagesIcon";
 import * as S from "@/components/containers/PostModal/PostModal.style";
 import Modal from "@/components/common/Modal/index";
 
-const MAX_LENGTH = 20;
+const MAX_LENGTH = 30;
 
 export default function PostModal({ subjectId, onClose, onSuccess }) {
   const [questionContent, setQuestionContent] = useState("");
@@ -30,11 +30,10 @@ export default function PostModal({ subjectId, onClose, onSuccess }) {
   const handleInputChange = (e) => {
     let { value } = e.target;
 
-    if (value.length > MAX_LENGTH) {
-      value = value.slice(0, MAX_LENGTH);
-    }
-    setQuestionContent(value);
+    if (value.length <= MAX_LENGTH) setQuestionContent(value);
   };
+
+  /* 질문이 입력되지 않았거나 공백일 때 버튼 비활성화 */
   const isButtonDisabled = questionContent.trim().length === 0;
 
   return (
@@ -65,6 +64,7 @@ export default function PostModal({ subjectId, onClose, onSuccess }) {
           placeholder="질문을 입력해주세요"
           value={questionContent}
           onChange={handleInputChange}
+          maxLength={MAX_LENGTH}
           autoFocus
         />
         <S.CharCount $isOver={questionContent.length > MAX_LENGTH}>
