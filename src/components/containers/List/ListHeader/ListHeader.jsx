fa8f3b8d809190e79e32a13ad8a7c92db8f56545
jiwonll <166104093+jiwonll@users.noreply.github.com>
2026-03-12 15:04:07 +0900

    fix:merge conflict 해결

--- src/components/containers/List/ListHeader/ListHeader.jsx
+++ src/components/containers/List/ListHeader/ListHeader.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 import * as S from "@/components/containers/List/ListHeader/ListHeader.style";
 import { SecondButton } from "@/components/common/Button/Button.style";
-import LogoImg from "@/assets/img/LogoImg";
+import {LogoImg} from "@/assets/img/LogoImg";
 import { ArrowRight2Icon } from "@/assets/icons/ArrowRight2Icon";
 
 export default function ListHeader() {
