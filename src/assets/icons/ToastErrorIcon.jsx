2e236fb70f0444fb23a305d9cf983dbb1c2a8a20
Hanpla <hanpla25@gmail.com>
2026-03-13 15:23:21 +0900

    chore: 토스트 아이콘 svg 좀 더 단순하게 다이어트

--- src/assets/icons/ToastErrorIcon.jsx
+++ src/assets/icons/ToastErrorIcon.jsx
@@ -6,24 +6,16 @@ export const ToastErrorIcon = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clipPath="url(#clip0_31985_3824)">
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M0.9375 9C0.9375 4.5472 4.5472 0.9375 9 0.9375C13.4528 0.9375 17.0625 4.5472 17.0625 9C17.0625 13.4528 13.4528 17.0625 9 17.0625C4.5472 17.0625 0.9375 13.4528 0.9375 9ZM9 2.0625C5.16852 2.0625 2.0625 5.16852 2.0625 9C2.0625 12.8315 5.16852 15.9375 9 15.9375C12.8315 15.9375 15.9375 12.8315 15.9375 9C15.9375 5.16852 12.8315 2.0625 9 2.0625Z"
-        fill="white"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M14.2504 15.0001L3.0004 3.75008L3.7959 2.95459L15.0459 14.2046L14.2504 15.0001Z"
-        fill="white"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_31985_3824">
-        <rect width="18" height="18" fill="white" />
-      </clipPath>
-    </defs>
+    <circle cx="9" cy="9" r="8" stroke="white" strokeWidth="1.2" />
+
+    <line
+      x1="4.5"
+      y1="4.5"
+      x2="13.5"
+      y2="13.5"
+      stroke="white"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+    />
   </svg>
 );
