3a206583fa405963b16b02a37313c6dd2b75642b
JuHeonParkk <ju10913@naver.com>
2026-03-15 21:39:36 +0900

    chore: 오타 수정

--- src/components/containers/Question/EmptyQuestion/EmptyQuestion.jsx
+++ src/components/containers/Question/EmptyQuestion/EmptyQuestion.jsx
@@ -6,7 +6,7 @@ export default function EmptyQuestion({ subjectData }) {
     <S.Container>
       <S.EmptyImg src={EmptyBox} alt="비어있는피드이미지" />
       <S.Message>
-        <span>{subjectData?.name || "프로필이름"}</span>님에게 <br />첫 질문을
+        <span>{subjectData?.name || "사용자"}</span>님에게 <br />첫 질문을
         남겨보세요!
       </S.Message>
     </S.Container>
