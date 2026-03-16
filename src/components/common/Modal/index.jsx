019a20262fc98c5b10863062b185ada8760a744f
pho9902 <bagh9902@gmail.com>
2026-03-16 16:36:13 +0900

    feat: 모달 바깥 영역 클릭시 닫는 기능 추가

--- src/components/common/Modal/index.jsx
+++ src/components/common/Modal/index.jsx
@@ -1,5 +1,13 @@
 import * as S from "./Modal.style";
 
 export default function Modal({ setIsOpen, children }) {
-  return <S.ModalWrapper>{children}</S.ModalWrapper>;
+  const handleWrapperClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <S.ModalWrapper onClick={handleWrapperClick}>{children}</S.ModalWrapper>
+  );
 }
