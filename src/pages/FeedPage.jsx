8cd0b92cdab98337ac484f57b9fab68a66fa364d
JuHeonParkk <ju10913@naver.com>
2026-03-16 10:31:22 +0900

    feat: 존재하지 않는 사용자에 대한 에러처리

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 import { subjectApi } from "@/apis/subject";
 import FeedHeader from "@/components/containers/Question/FeedHeader/FeedHeader";
 import QuestionList from "@/components/containers/Question/QuestionList";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 
 import * as S from "@/components/containers/Question/QuestionList.style";
 
 export default function FeedPage() {
   const [subjectData, setSubjectData] = useState(null);
+  const [isError, setIsError] = useState(null);
 
   const navigate = useNavigate();
   const { subjectId, "*": subpath } = useParams();
@@ -28,12 +30,18 @@ export default function FeedPage() {
         const data = await subjectApi.getFeedData(subjectId);
         setSubjectData(data);
       } catch (error) {
-        // Todo: 에러 핸들링 UI 추가
+        setIsError(
+          error.response?.data?.detail || "데이터를 가져오는데 실패했습니다.",
+        );
         console.error("Error fetching subject data:", error);
       }
     };
     fetchSubjectData();
   }, [subjectId]);
+
+  if (isError) {
+    return <NotFoundPage />;
+  }
 
   return (
     <>
