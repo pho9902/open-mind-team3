878e18f49a73c3641e2dec92ddea59a725959853
JuHeonParkk <ju10913@naver.com>
2026-03-17 16:13:13 +0900

    chore: question api에 questionId 추가

--- src/apis/question.js
+++ src/apis/question.js
@@ -2,14 +2,17 @@ import instance from "@/apis/instance";
 
 export const questionApi = {
   // 개별 질문 조회
-  getQuestion: (questionId) => instance.get(`questions/${questionId}/`),
+  getQuestion: (questionId) =>
+    instance.get(`questions/${questionId}/`, {
+      params: { questionId },
+    }),
 
   // 개별 질문 삭제
   deleteQuestion: (questionId) => instance.delete(`questions/${questionId}/`),
 
   // 질문 리액션 등록
   createReaction: (questionId, type) =>
-    instance.post(`questions/${questionId}/reaction/`, { type }),
+    instance.post(`questions/${questionId}/reaction/`, { questionId, type }),
 
   // 질문 답변 등록
   createAnswer: (questionId, content) =>
